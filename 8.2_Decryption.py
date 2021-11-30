a6f7df60c888093d01e485772e53a8ad024b8560
Marc Hermon <hermonm@urbandaleschools.com>
2021-11-30 11:52:07 -0600

    Update 8.2_Decryption.py

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -1,13 +1,14 @@
 '''
 DECRYPTION PROGRAM
 ------------------
-An encryption program was used to generate the following secret code. The encryption program converted each character 
+An ENCRYPTION program was used to generate the following secret code. The encryption program converted each character 
 of the string into its ASCII decimal number, applied a +/-20 algorithm to it and then converted it back to
-characters. Your task is to write a Decryption program to decipher the following secret code. Don't waste time changing 
+characters. Your task is to write a DECRYPTION program to decipher the following secret code. Don't waste time changing 
 your program 40 times. Use a FOR loop from -20 to +20 to generate all the possibilities in one run of your program.
 
-Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
+Extra Challenge: Instead of printing out 41 lines of text to look at, can you determine a way to just print out the decrypted line only
+along with the shift number?
 '''
-
+Secret_Message="Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
 
