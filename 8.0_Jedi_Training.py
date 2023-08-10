cc79b3a477254d9ab2a40c6b93efd967da50529a
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-10 09:04:43 -0500

    Update 8.0_Jedi_Training.py

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -1,7 +1,7 @@
 # 8.0 Jedi Training (20pts)  Name:________________
 
 '''
-1.)
+1.)  AVERAGE LIST:  (3PTS)
 Write a single program that takes any of the three lists, and prints the average. 
 Use the len function. There is a sum function I haven't told you about. 
 Don't use that. Sum the numbers individually as shown in the chapter. 
@@ -15,14 +15,15 @@ c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
 
 '''
-2.) Write a program that will strip the username (whatever is in front of the @ symbol)
+2.) USERNAME:  (3PTS)
+Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
 
 
 
 '''
-TEXT FORMATTING:
+TEXT FORMATTING:  (4PTS)
 3.) Make following program output the following:
      
      Score:          41,237
@@ -38,8 +39,7 @@ print("High score: " + str(highscore) )
 
 
 '''
-4.) MONTHS PROGRAM
---------------
+4.) MONTHS PROGRAM   (5PTS)
 Write a user-input statement where a user enters a month number 1-12.
 From the user input number, slice the string below in your program to print the three month abbreviation.
 Keep repeating this until the user enters a non 1-12 number to quit.
@@ -49,4 +49,16 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 
+
+'''
+5.) DECRYPTION PROGRAM   (5PTS)
+An ENCRYPTION program was used to generate the following secret code. The encryption program converted each character 
+of the string into its ASCII decimal number, applied a +/-20 algorithm to it and then converted it back to
+characters. Your task is to write a DECRYPTION program to decipher the following secret code. Don't waste time changing 
+your program 40 times. Use a FOR loop from -20 to +20 to generate all the possibilities in one run of your program.
+
+Extra Challenge: Instead of printing out 41 lines of text to look at, can you determine a way to just print out the decrypted line only
+along with the shift number?
+'''
+Secret_Message="Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
