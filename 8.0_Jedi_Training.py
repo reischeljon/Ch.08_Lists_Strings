eed3f98879d6d337b0ed2db457e6725fedc130ab
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-10 09:01:57 -0500

    Update 8.0_Jedi_Training.py

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-#Sign your name:________________
+# 8.0 Jedi Training (20pts)  Name:________________
 
 '''
 1.)
@@ -35,5 +35,18 @@ score = 41237
 highscore = 1023407
 print("Score:      " + str(score) )
 print("High score: " + str(highscore) )
+
+
+'''
+4.) MONTHS PROGRAM
+--------------
+Write a user-input statement where a user enters a month number 1-12.
+From the user input number, slice the string below in your program to print the three month abbreviation.
+Keep repeating this until the user enters a non 1-12 number to quit.
+Once the user quits, print "Goodbye!"
+'''
+
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+
 
 
